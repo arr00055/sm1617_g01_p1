e6d74b252fa0f883738611a89c99e9be24b5b093
arr00055 <arr00055@red.ujaen.es>
2016-09-29 12:21:03 +0200

    Sesión 1

--- practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
+++ practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
@@ -1,5 +1,6 @@
 package es.ujaen.git.practica1;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_aut);
+    }
+
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return super.getSupportFragmentManager();
     }
 }
